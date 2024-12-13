7ef56e6cac5b764b332f7092f4a730e3a26686c2
Tobless-scripts <obayomitaofeek7@gmail.com>
2024-12-12 17:11:06 -0800

    repo continuation

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -19,3 +19,24 @@ window.onscroll = function () {
     }
 
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target)
+            } else {
+                entry.target.classList.remove('visible');
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    const elements = document.querySelectorAll(".trackScroll");
+    elements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
